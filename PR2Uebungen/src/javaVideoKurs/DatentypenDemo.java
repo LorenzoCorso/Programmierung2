04fca58f59b197a9cb41470fe7344dd3b72804c3
lorenzocorso <lorenzocorso@lorenzos-imac>
2024-04-07 15:29:53 +0200

    Überarbteiung

---
+++ PR2Uebungen/src/javaVideoKurs/DatentypenDemo.java
@@ -0,0 +1,26 @@
+package javaVideoKurs;
+
+public class DatentypenDemo {
+
+	public static void main(String[] args) {
+
+		// primitive Datentypen
+
+		int i = 25;
+		System.out.println(Integer.MIN_VALUE); // Methode welche den kleinsten möglichen Wert ausgibt welchen man in
+												// einem Integer speichern kann
+		long l = 500000000000L;
+		short s = 50;
+		byte b = 20;
+		System.out.println(Byte.MAX_VALUE); // => Genau wie oben & geht mit jeden von den Datentypen hier
+
+		float f = 2.5F;
+		double d = 2.5345983;
+
+		char c = 'A';
+
+		boolean bool = false;
+
+	}
+
+}
