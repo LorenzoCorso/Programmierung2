6223593c44250c9b6509ad266dcbcf544b730f3d
lorenzocorso <lorenzocorso@lorenzos-imac>
2024-04-09 00:35:45 +0200

    Kommentare in die Klasse Abschnitt5CallByValue.java hinzugefügt

--- PR2Uebungen/src/javaVideoKurs/Abschnitt5CallByValue.java
+++ PR2Uebungen/src/javaVideoKurs/Abschnitt5CallByValue.java
@@ -4,13 +4,13 @@ public class Abschnitt5CallByValue {
 
 	public static void main(String[] args) {
 
-		XY xy = new XY();
-		initializeXY(xy);
-		System.out.println("X = " + xy.x + ", y = " + xy.y);
+		XY xy = new XY();    // Lokale Variablen welcher hier zuvor standen => int x = 0; int y = 0; wurden in die Klasse XY verlagert => Objket erstellen in welcher die Daten gekapslet sind 
+		initializeXY(xy);    // Aufruf der Methode 
+		System.out.println("X = " + xy.x + ", y = " + xy.y);// Übergabe der in der neuen festgelegten Varibalen in der Methode => zeigen nun nicht mehr auf Referzenz von Default (Klasse XY) sondern auf das zugewiesene hier 
 
 	}
 
-	private static void initializeXY(XY xy) {
+	private static void initializeXY(XY xy) { // Wir übergeben das Objekt und ändern die Referenz auf welche die Varibalen innerhalb des Objektes zeigen !
 		xy.x = 5;
 		xy.y = 8;
 	}
