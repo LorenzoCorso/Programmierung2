f45ce5bea60bdbf35e04579abe91bbb95dc4dfc9
lorenzocorso <lorenzocorso@lorenzos-imac>
2024-04-14 18:47:54 +0200

    popopopo

---
+++ PR2Uebungen/src/javaVideoKurs/VergleichsOperatorenDemo.java
@@ -0,0 +1,40 @@
+package javaVideoKurs;
+
+public class VergleichsOperatorenDemo {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * 
+		 * <
+		 * >
+		 * >=
+		 * <=
+		 * == 
+		 * !=
+		 * 
+		 */
+		
+		double x = 6;
+		long y = 3;
+		
+		boolean b = x < y;
+		System.out.println(b);  // Impliziter Cast von Long in Double
+		
+		boolean b2 = true == false;   // Vergleicht ob ein Wert mit dem anderen übereinstimmt
+		System.out.println(b2);
+		
+		
+		boolean b3 = true != false;
+		System.out.println(b3);
+		
+		boolean b4 = "Hallo" == "Ciao";
+		System.out.println(b4);
+
+		Auto a4 = new Auto (100, "BMW");
+		Auto a5 = new Auto(100, "BMW");
+		System.out.println(a4 == a5);   // Wieso kommt hier false raus => Wir erstellen zwei neue Auto mit den gleichen Eigenschaften jedoch sind die Objekte in anderen Referenzen gespeichert, d.h. wir verlgeichen nicht das Objekt selbst sonder die Referenz => Prüfung auf Identität wird hier gemacht !!
+	}
+
+}
