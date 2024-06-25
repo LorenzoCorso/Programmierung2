d5e0543f60b70c3e4870cd17efc543dba8afec7b
lorenzo <lorenzo@mbp-von-lorenzo-3>
2024-06-25 16:47:44 +0200

    as

--- PR2Uebungen/src/javaVideoKurs/Schaltjahr.java
+++ PR2Uebungen/src/javaVideoKurs/Schaltjahr.java
@@ -1,26 +1,11 @@
 package javaVideoKurs;
 
-import java.util.Scanner;
-
 public class Schaltjahr {
-
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		
-		System.out.println("Bitte geben Sie eine Jahreszahl ein");
-		
-		int jahr = sc.nextInt(); 
-		boolean istSchaltjahr = (jahr % 4 == 0) && (jahr % 100 != 0) || (jahr % 400 == 0);
+		int jahr = 1404;
 
-		if (istSchaltjahr) {
-			System.out.println(jahr + " ist ein Schaltjahr.");
-		} else {
-			System.out.println(jahr + " ist kein Schaltjahr.");
-			
-		
-		}
-		sc.close();
+		boolean schaltjahr = (jahr % 4 == 0 && (jahr % 400 == 0 || jahr % 100 != 0));
+
+		System.out.println("Das Jahr " + jahr + " ist " + (!schaltjahr ? "k" : "") + "ein Schaltjahr");
 	}
 }
